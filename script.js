6a6e81db82f6af9ec49492c33b6f7b819ae00eb4
Eric Brown <ericbrown589@yahoo.com>
2021-11-30 14:59:19 -0600

    added comments explaining what the code does

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ click restart
 */
 
 const gameSpace = document.querySelector(".gamespace")
-const gameTiles = Array.from(document.querySelectorAll(".tiles"))
+const gameTiles = document.querySelectorAll(".tiles")
 const status = document.querySelector("#status")
 const restart = document.querySelector("#restart")
 const start = document.querySelector("#start")
@@ -31,10 +31,10 @@ const x_won = "Player One Won!"
 const o_won = "Player Two Won!"
 const draw = "It's a draw!"
 
-start.addEventListener("click", function(event){
+start.addEventListener("click", function(event) {
     event.preventDefault
     return gameStarted = true
-})
+}) //start button
 
 const isValid = function(tiles) {
     if (tiles.innerText === "X" || tiles.innerText === "O"){
@@ -42,14 +42,14 @@ const isValid = function(tiles) {
     } else {
         return true
     }
-}
+} //checks to see if the tile is empty or not
 
 const boardUpdate = function(index) {
     gameBoard[index] = currentPlayer
-}
+} //updates the board with current player letter
 
 const changePlayer = function() {
-    if(currentPlayer === "X"){
+    if(currentPlayer === "X") {
         document.getElementById("one").classList.remove("currentPlayer")
         document.getElementById("two").classList.add("currentPlayer")
         return currentPlayer = "O"
@@ -58,59 +58,59 @@ const changePlayer = function() {
         document.getElementById("one").classList.add("currentPlayer")
         return currentPlayer = "X"
     }
-}
+} //changes player from x to o back to x
 
 function winCheck() {
-    if(gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X"){
+    if(gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O") {
         document.getElementById("status").innerText = o_won
         return gameStarted = false
     } 
-    if(gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X"){
+    if(gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O") {
         document.getElementById("status").innerText = o_won
         return gameStarted = false
     }
-    if(gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X"){
+    if(gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O") {
         document.getElementById("status").innerText = o_won
         return gameStarted = false
     }
-    if(gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X"){
+    if(gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O") {
         document.getElementById("status").innerText = o_won
         return gameStarted = false
     }
-    if(gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X"){
+    if(gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard[7] === "O") {
         document.getElementById("status").innerText = o_won
         return gameStarted = false
     }
-    if(gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X"){
+    if(gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O") {
         document.getElementById("status").innerText = o_won
         return gameStarted = false
     }
-    if(gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X"){
+    if(gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[0] === "O" && gameBoard[4] === "O" && gameBoard[8] === "O") {
         document.getElementById("status").innerText = o_won
         return gameStarted = false
     }
-    if(gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X"){
+    if(gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X") {
         document.getElementById("status").innerText = x_won
         return gameStarted = false
     } else if(gameBoard[2] === "O" && gameBoard[4] === "O" && gameBoard[6] === "O") {
@@ -120,7 +120,7 @@ function winCheck() {
     if (!gameBoard.includes("")) {
         document.getElementById("status").innerText = draw
     }
-}
+} //checks to see if theres a win condition, if there is it will change the status tag in the html to have a text of the winner or if there is a draw 
 
 
 const tileClick = function(tiles, index) {
@@ -130,25 +130,24 @@ const tileClick = function(tiles, index) {
         winCheck()
         changePlayer()
     }
-}
+} //says what will happen when a tile is click
 
-gameTiles.forEach(function(tiles, index){
-    tiles.addEventListener("click", function(){
+gameTiles.forEach(function(tiles, index) {
+    tiles.addEventListener("click", function() {
         tileClick(tiles, index)
     })
-})
+}) //adds the functionality to click the tiles and runs the tile click checks
 
 
-restart.addEventListener("click", function(event){
+restart.addEventListener("click", function(event) {
     event.preventDefault
     gameBoard = ["", "", "", "", "", "", "", "", ""]
     if (currentPlayer === "O") {
         changePlayer()
     }
-    gameTiles.forEach(function(tiles){
-        tiles.innerText = " "
+    gameTiles.forEach(function(tiles) {
+        tiles.innerText = ""
     })
     gameStarted = false
-
     document.getElementById("status").innerText = ""
-})
+}) //restarts the game without having to refresh the page
